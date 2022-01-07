a3d04213a474dde345c3e5c9ae52f7a340ff21d1
anass-knene <anas.knene@hotmail.com>
2022-01-07 10:57:22 +0100

    create server

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const server = express();
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const PORT = process.env.PORT;
+const {
+  movies,
+  newMovies,
+  actionMovies,
+  getMovie,
+} = require("./controllers/movies");
+// 1 all movies route
+server.get("/movies", movies);
+// 2 new movies route
+server.get("/new-movies", newMovies);
+// 3 action movies route
+server.get("/action-movies", actionMovies);
 
-server.get("/movies", (req, res) => {
-  res.send("Movies");
-});
-server.get("/tv-shows", (req, res) => {
-  res.send("Tv-Shows");
-});
+//  3 find movie by id
+
+server.get("/movie/:id", getMovie);
+
+//  params ==> /:id
+//  queries ==> ?id=1234
+// server.get("/tv-shows", (req, res) => {
+//   res.send("Tv-Shows");
+// });
 
 server.listen(PORT, () => {
   console.log(`Anass Netflix server running on port ${PORT}`);
